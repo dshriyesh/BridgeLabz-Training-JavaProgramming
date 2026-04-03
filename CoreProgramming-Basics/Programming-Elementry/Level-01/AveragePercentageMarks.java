65e721440cecd8479a9b13fc203eb2ac1acd379e
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-03 12:00:22 +0530

    [Shriyesh dixit] added code to find Average percentage of the marks obtained

---
+++ CoreProgramming-Basics/Programming-Elementry/Level-01/AveragePercentageMarks.java
@@ -0,0 +1,14 @@
+// Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find the average
+// percent mark in PCM
+public class AveragePercentageMarks {
+    public static void main(String[] args) {
+        int mathsMark = 94;
+        int physicsMark = 95;
+        int ChemMark = 96;
+
+        int totalObtainedMark = (94+95+96);
+
+        System.out.println("Sam's average mark in PCM is : "+(totalObtainedMark/3));
+
+    }
+}

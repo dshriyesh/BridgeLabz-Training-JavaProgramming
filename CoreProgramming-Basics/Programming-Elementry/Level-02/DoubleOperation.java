87c469f32d6737eb62aa4dd1ba220da0ea881eab
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-03 14:42:00 +0530

    [Shriyesh dixit] added code for calculating airthematic operations between different values

---
+++ CoreProgramming-Basics/Programming-Elementry/Level-02/DoubleOperation.java
@@ -0,0 +1,23 @@
+// write the DoubleOpt program by taking double values and doing the same
+// operations.
+import java.util.Scanner;
+public class DoubleOperation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter double value for a: ");
+        double a = input.nextDouble();
+        System.out.print("Enter double value for b: ");
+        double b = input.nextDouble();
+        System.out.print("Enter double value for c: ");
+        double c = input.nextDouble();
+
+        double res1 = a + b * c;   
+        double res2 = a * b + c;   
+        double res3 = c + a / b;   
+        double res4 = a % b + c;   
+
+        System.out.println("The results of Double Operations are " 
+            + res1 + ", " + res2 + ", " + res3 + ", and " + res4);
+    }
+}

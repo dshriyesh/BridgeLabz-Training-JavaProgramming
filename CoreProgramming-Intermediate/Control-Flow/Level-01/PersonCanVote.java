eb7ba53e811ac229f6930c48fe0729a34f1f1f8d
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-04 10:57:57 +0530

    [Shriyesh dixit] added code to check weather a person can vote or not

---
+++ CoreProgramming-Intermediate/Control-Flow/Level-01/PersonCanVote.java
@@ -0,0 +1,18 @@
+// Write a program to check whether a person can vote, depending on whether his/her age is
+// greater than or equal to 18.
+import java.util.Scanner;
+public class PersonCanVote {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        //  check for voting eligibility
+        if (age >= 18) {
+            System.out.println("The person's age is " + age + " and can vote.");
+        } else {
+            System.out.println("The person's age is " + age + " and cannot vote.");
+        }
+    }
+}

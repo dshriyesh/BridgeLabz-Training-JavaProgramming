6b9b14729308e7e684e3997b8d138dc3da2bc880
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-14 01:42:27 +0530

    [Shriyesh dixit] added code for default constructors

---
+++ Object-OrientedProgramming/OOP-Basics/Constructors/DefaultConstructor.java
@@ -0,0 +1,10 @@
+public class DefaultConstructor {
+        public DefaultConstructor ()
+    {
+        System.out.println ("This is a no-argument constructor");
+    }
+    public static void main (String args[])
+    {
+        new DefaultConstructor();
+    }
+}

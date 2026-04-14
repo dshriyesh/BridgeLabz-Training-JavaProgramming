0a2c544edff7bdb30673d7f89d309fdf8b920919
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-14 14:36:37 +0530

    [Shriyesh dixit] added code for multilevel inheritance

---
+++ Object-OrientedProgramming/OOP-Basics/Inheritance/MultiLevelInheritance.java
@@ -0,0 +1,26 @@
+public class MultiLevelInheritance { 
+    public static void main(String[] args) { 
+        A obj = new A(); 
+        A.fun(); 
+        B objB = new B();
+        objB.fun();
+    } 
+} 
+
+class A { 
+    public static void fun() { 
+        System.out.println(" Class A method "); 
+    } 
+} 
+
+class B extends A { 
+    public static void fun() { 
+        System.out.println(" Class B method "); 
+    } 
+} 
+
+class C extends B{ 
+    public static void fun() { 
+        System.out.println(" Class C method "); 
+    } 
+}

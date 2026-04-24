e2700a13b2e629b70fb5a3440d4e0b2f9deae645
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-24 12:15:57 +0530

    [Shriyesh dixit] added code for generics Upper bounded wildcards.

---
+++ Advanced-Java-Programming/JavaCollections/Generics/UpperBound.java
@@ -0,0 +1,23 @@
+// used to resolve problems faced in unbounded type
+// it extends the sub-type of a class to implement the functions
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UpperBound {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        System.out.println(listSum(list1));
+    }
+    public static double listSum(ArrayList<? extends Number> numbers){
+        double sum=0;
+        for(Number o:numbers){
+            sum+=o.doubleValue();
+        }
+        return sum;
+    }
+}

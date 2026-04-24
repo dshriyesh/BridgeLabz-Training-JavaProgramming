4aa0a1ac701faab1a284603abdae2999fb160392
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-24 12:00:54 +0530

    [Shriyesh dixit] added code for generics Unbounded wildcards.

--- Advanced-Java-Programming/JavaCollections/Generics/UnboundedWildcards.java
+++ Advanced-Java-Programming/JavaCollections/Generics/UnboundedWildcards.java
@@ -1,3 +1,8 @@
+// mostly used in places where we are doing read only task not returning something or not copying something
+// bcz causes type safety errors
+// used where the parameter type is undefined
+
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,10 +12,18 @@ public class UnboundedWildcards {
         List<String>  ls = Arrays.asList("one", "two", "three");
         printList(li);
         printList(ls);
+
+        // copy(Arrays.asList(1,2,3),Arrays.asList()); // throws error due to type mismatch
     }
-    public static void printList(List<?> list) {
+    public static void printList(List<?> list) { // read only task
     for (Object elem: list)
         IO.print(elem + " ");
     IO.println();
-}
+ }
+
+//  public void copy(ArrayList<?> source, ArrayList<?> dest){
+//     for(Object o:source){
+//         dest.add(o);
+//     }
+//  } //causes error
 }

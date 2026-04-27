32705255fb3e02a927542b9f5d738926b00f48c5
Shriyesh Dixit <shriyeshdixit@gmail.com>
2026-04-27 10:36:57 +0530

    [Shriyesh dixit] added code for Hahmap collections.

---
+++ Advanced-Java-Programming/JavaCollections/Collections-Framework/Map/HashMapExample.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "Apple");
+        hashMap.put(2, "Banana");
+        hashMap.put(3, "Cherry");
+
+        // Iterating using enhanced for loop
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Iterating using iterator
+        Iterator<Map.Entry<Integer, String>> iterator = hashMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+}
